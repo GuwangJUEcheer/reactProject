3c7de64acc45cd7a0fa7542e8afedf17b1ece69c
MYCOMPUTER\17685 <yiyi5205211314@gmail.com>
2024-03-02 20:13:11 +0900

    注册和登录页面

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,33 +1,29 @@
 // App.js
 import React from 'react';
-import { Route, Switch, NavLink } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
+import { Route, Routes, NavLink, Navigate } from 'react-router-dom';
+import Login from './component/login';
+import Register from './component/register';
 
 function App() {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/login" activeClassName="active">Login</NavLink>
+      <nav style={{ backgroundColor: '#333', overflow: 'hidden' }}>
+        <ul style={{ listStyleType: 'none', margin: '0', padding: '0', display: 'flex', justifyContent: 'center' }}>
+          <li style={{ float: 'left' }}>
+            <NavLink to="/login" style={{ color: 'white', textAlign: 'center', padding: '14px 16px', textDecoration: 'none', fontSize: '17px' }}>Login</NavLink>
           </li>
-          <li>
-            <NavLink to="/register" activeClassName="active">Register</NavLink>
+          <li style={{ float: 'left' }}>
+            <NavLink to="/register" style={{ color: 'white', textAlign: 'center', padding: '14px 16px', textDecoration: 'none', fontSize: '17px' }}>Register</NavLink>
           </li>
         </ul>
       </nav>
 
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
-
 export default App;
